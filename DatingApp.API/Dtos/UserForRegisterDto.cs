8d34329642aa9daa2454e897d4d75c3863f42b0d
Han Co <lhanco@gmail.com>
2018-09-21 22:15:42 +0700

    Using Token Authentication

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {        
+        [Required]
+        [StringLength(50, 
+        MinimumLength = 5,
+        ErrorMessage = "You must specify user name between 5 and 50 characters.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, 
+        MinimumLength = 4,
+        ErrorMessage = "You must specify password between 4 and 20 characters.")]
+        public string Password { get; set; }
+        
+    }
+}
