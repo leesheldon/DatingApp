1686c2509cc93720df831435da15c3890e443d4e
Han Co <lhanco@gmail.com>
2018-09-28 17:02:10 +0700

    Update User Profile

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 export const appRoutes: Routes = [
@@ -18,8 +21,11 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, 
                 resolve: { users: MemberListResolver } },
-            { path: 'members/:id', component: MemberDetailComponent, 
-                resolve: { user: MemberDetailResolver } },
+            { path: 'member/edit', component: MemberEditComponent, 
+                resolve: { user: MemberEditResolver },
+                canDeactivate: [PreventUnsavedChanges] },      
+            { path: 'member/:id', component: MemberDetailComponent, 
+                resolve: { user: MemberDetailResolver } },            
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent }
         ]
